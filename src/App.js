8b4844ab9e04935e510829d6878764571aff7ad0
Z-Mode7z <132988263+Z-Mode7z@users.noreply.github.com>
2024-03-14 13:51:29 +0530

    Added reset button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(0);
+  const [currentTime, setCurrentTime] = useState(new Date());
 
-  const currentDate = new Date();
-  const updatedCount = count + step;
-  currentDate.setDate(currentDate.getDate() + updatedCount);
-  const currentTime = currentDate.toLocaleDateString();
+  useEffect(() => {
+    const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + count);
+    setCurrentTime(currentDate);
+  }, [count]);
+
+  const incrementCount = () => {
+    setCount((prevCount) => step === 0 ? prevCount + 1 : prevCount + step);
+  };
+
+  const decrementCount = () => {
+    setCount((prevCount) => step === 0 ? prevCount - 1 : prevCount - step);
+  };
 
   return (
     <div className="App">
       <div>
         <button onClick={() => setStep((prevStep) => prevStep + 1)}>+</button>
-        <h2>Step: {step}</h2>
-        <button>-</button>
+        <label>Step: {step}</label>
+        <button onClick={() => setStep((prevStep) => prevStep - 1)}>-</button>
       </div>
       <div>
-        <button onClick={() => setCount((prevCount) => prevCount + 1)}>+</button>
+        <button onClick={incrementCount}>+</button>
         <h2>Count: {count}</h2>
-        <button onClick={() => setCount((prevCount) => prevCount - 1)}>-</button>
+        <button onClick={decrementCount}>-</button>
       </div>
       <div>
-        Today is: <b>{currentTime}</b>
+        <button onClick={() => setCount(0)}>Reset</button>
+        Today is: <b>{currentTime.toLocaleDateString()}</b>
       </div>
     </div>
   );
